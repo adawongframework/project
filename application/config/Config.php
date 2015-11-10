a30f92d4a977f7a5431bc35085290f68eff2f992
zhangjie <zhangjiecyhy@sian.com>
2015-11-10 17:21:40 +0800

    modfiy framework file

--- application/config/Config.php
+++ application/config/Config.php
@@ -1,6 +1,9 @@
 <?php
+/**
+* 项目配置文件
+*+------------
+*/
 return Array(
-	'baseurl' => 'http://localhost/project/',
-	'indexfile' => ''
+	'baseurl' => 'http://localhost/project/', //服务器地址
+	'indexfile' => '' //入口文件名称
 );
-?>
